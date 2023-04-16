2b9445ad65a07e516d63115f0cce96f35d65bb43
DemetriusADS <demetriusegundo@gmail.com>
2023-04-16 10:21:12 -0300

    fix: ajusta o layout

--- src/components/Cadastrar_Cliente.vue
+++ src/components/Cadastrar_Cliente.vue
@@ -1,122 +1,132 @@
 <template>
-    
-    <v-main style="min-height: 768px; display: flex" class="Content">
-            <form>
-    <v-text-field
-      v-model="state.name"
-      :error-messages="v$.name.$errors.map(e => e.$message)"
-      :counter="10"
-      label="Name"
-      required
-      @input="v$.name.$touch"
-      @blur="v$.name.$touch"
-    ></v-text-field>
+  <!-- <form>
+      <v-text-field
+        v-model="state.name"
+        :error-messages="v$.name.$errors.map((e) => e.$message)"
+        :counter="10"
+        label="Name"
+        required
+        @input="v$.name.$touch"
+        @blur="v$.name.$touch"
+      ></v-text-field>
 
-    <v-text-field
-      v-model="state.email"
-      :error-messages="v$.email.$errors.map(e => e.$message)"
-      label="E-mail"
-      required
-      @input="v$.email.$touch"
-      @blur="v$.email.$touch"
-      colo
-    ></v-text-field>
+      <v-text-field
+        v-model="state.email"
+        :error-messages="v$.email.$errors.map((e) => e.$message)"
+        label="E-mail"
+        required
+        @input="v$.email.$touch"
+        @blur="v$.email.$touch"
+        colo
+      ></v-text-field>
 
-    <v-select
-      v-model="state.select"
-      :items="items"
-      :error-messages="v$.select.$errors.map(e => e.$message)"
-      label="Item"
-      required
-      @change="v$.select.$touch"
-      @blur="v$.select.$touch"
-    ></v-select>
+      <v-select
+        v-model="state.select"
+        :items="items"
+        :error-messages="v$.select.$errors.map((e) => e.$message)"
+        label="Item"
+        required
+        @change="v$.select.$touch"
+        @blur="v$.select.$touch"
+      ></v-select>
 
-    <v-checkbox
-      v-model="state.checkbox"
-      :error-messages="v$.checkbox.$errors.map(e => e.$message)"
-      label="Do you agree?"
-      required
-      @change="v$.checkbox.$touch"
-      @blur="v$.checkbox.$touch"
-    ></v-checkbox>
+      <v-checkbox
+        v-model="state.checkbox"
+        :error-messages="v$.checkbox.$errors.map((e) => e.$message)"
+        label="Do you agree?"
+        required
+        @change="v$.checkbox.$touch"
+        @blur="v$.checkbox.$touch"
+      ></v-checkbox>
 
-    <v-btn
-      class="me-4"
-      @click="v$.$validate"
-    >
-      submit
-    </v-btn>
-    <v-btn @click="clear">
-      clear
-    </v-btn>
-  </form>
-        </v-main>    
-    
+      <v-btn class="me-4" @click="v$.$validate"> submit </v-btn>
+      <v-btn @click="clear"> clear </v-btn>
+    </form> -->
+
+  centrilize in the middle of the screen
+  <v-card class="w-50 mx-auto mt-12">
+    <v-form v-model="valid" class="w-100">
+      <v-container>
+        <v-row>
+          <v-col cols="12">
+            <v-text-field
+              v-model="firstname"
+              :rules="nameRules"
+              :counter="10"
+              label="Nome"
+              required
+            ></v-text-field>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" md="6">
+            <v-text-field
+              v-model="lastname"
+              :rules="nameRules"
+              :counter="10"
+              label="Sobrenome"
+              required
+            ></v-text-field>
+          </v-col>
+
+          <v-col cols="12" md="6">
+            <v-text-field
+              v-model="email"
+              :rules="emailRules"
+              label="Celular"
+              required
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-form>
+  </v-card>
 </template>
 
-<script >
-import App from '../App.vue'
-import Vue from 'vue'
+<script>
+import App from "../App.vue";
+import Vue from "vue";
 
+import { reactive, ref } from "vue";
+import { useVuelidate } from "@vuelidate/core";
+import { email, required } from "@vuelidate/validators";
 
-import { reactive, ref } from 'vue'
-import { useVuelidate } from '@vuelidate/core'
-import { email, required } from '@vuelidate/validators'
+export default {
+  setup() {
+    const initialState = {
+      name: "",
+      email: "",
+      select: null,
+      checkbox: null,
+    };
 
-  export default {
+    const state = reactive({
+      ...initialState,
+    });
 
-    setup () {
-      const initialState = {
-        name: '',
-        email: '',
-        select: null,
-        checkbox: null,
+    const items = ref(["Item 1", "Item 2", "Item 3", "Item 4"]);
+
+    const rules = {
+      name: { required },
+      email: { required, email },
+      select: { required },
+      items: { required },
+      checkbox: { required },
+    };
+
+    const v$ = useVuelidate(rules, state);
+
+    function clear() {
+      v$.value.$reset();
+
+      for (const [key, value] of Object.entries(initialState)) {
+        state[key] = value;
       }
+    }
 
-      const state = reactive({
-        ...initialState,
-      })
-
-      const items = ref([
-        'Item 1',
-        'Item 2',
-        'Item 3',
-        'Item 4',
-      ])
-
-      const rules = {
-        name: { required },
-        email: { required, email },
-        select: { required },
-        items: { required },
-        checkbox: { required },
-      }
-
-      const v$ = useVuelidate(rules, state)
-
-      function clear () {
-        v$.value.$reset()
-
-        for (const [key, value] of Object.entries(initialState)) {
-          state[key] = value
-        }
-      }
-
-      return { state, items, clear, v$ }
-    },
-  }   
-
-
+    return { state, items, clear, v$ };
+  },
+};
 </script>
 
-<style scoped>
-.Content2{
-  background-color: black;
-  opacity: 90%;
-  background-image: url("../assets/BackGround.jpg");
-  background-size:auto;
-  margin-left: 180px;
-}
-
-</style>
+<style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-import 'vuetify/styles'
-import {createVuetify} from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-import { Vue } from 'vue-class-component'
-import '@mdi/font/css/materialdesignicons.css'
-
-
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import { Vue } from "vue-class-component";
+import "@mdi/font/css/materialdesignicons.css";
 
 const vuetify = createVuetify({
-    components,
-    directives
-})
+  components,
+  directives,
+  theme: {
+    defaultTheme: "light",
+  },
+});
 
-
-
-
-
-
-createApp(App).use(vuetify).use(router).mount('#app')
-
-
+createApp(App).use(vuetify).use(router).mount("#app");
